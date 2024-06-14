975b40c1588870fe608e0d6d2ee44af31fbca3ba
chandrakanth93986 <chandu93986@gmail.com>
2024-06-14 23:47:07 +0530

    Created register route

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,11 +6,15 @@ const userSchema = new Schema({
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
         required: true
+    },
+    address: {
+        type: String
     }
 });
 
--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -2,18 +2,31 @@ import express from 'express'
 const userRoute = express.Router()
 import User from '../models/User.js'
 import expressAsyncHandler from 'express-async-handler'
+import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 userRoute.post("/register", expressAsyncHandler(async (req, res) => {
     try {
         // check if the user exists and create if doesnot exists
-        const { email } = req.body;
+        const userData = req.body;
+        const { email, password } = userData;
         const existingUser = await User.findOne({ email })
         if (existingUser) {
             return res.status(400).json({ message: "User exists!" })
         }
+        const hashedPassword = await bcryptjs.hash(password, 10);
+        userData.password = hashedPassword
 
-        const newUser = await User(req.body);
+        const newUser = await User(userData);
         await newUser.save()
+
+        const token = jwt.sign({ email: email }, process.env.JWT_SECRET_KEY, { expiresIn: '1d' })
+
+        res.cookie("auth_token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            maxAge: 86400000
+        })
 
         return res.status(201).json({ message: "User Created Successfully!", newUser })
     } catch (error) {
