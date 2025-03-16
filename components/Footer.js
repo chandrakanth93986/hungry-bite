627e030c6c1066175cf8647dcc403f7d8075794c
chandrakanth93986 <chandu93986@gmail.com>
2025-03-16 23:43:18 +0530

    Navbar and Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,37 @@
-'use client'
-import React from 'react'
+"use client";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+    return (
+        <footer className="bg-primary text-white">
+            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                <span className="text-sm sm:text-center">
+                    @Hungry-Bite. All Rights Reserved.
+                </span>
+                <ul className="flex flex-wrap items-center mt-3 text-sm font-medium sm:mt-0">
+                    <li>
+                        <a href="#" className="hover:underline me-4 md:me-6">
+                            About
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:underline me-4 md:me-6">
+                            Privacy Policy
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:underline me-4 md:me-6">
+                            Licensing
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:underline">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,66 @@
-'use client'
+'use client';
+
 import React from 'react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import logo from '@/public/logo.png'
+import Image from 'next/image';
+import { useSession } from 'next-auth/react';
+import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { MdWavingHand } from "react-icons/md";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    const router = useRouter()
+    const path = usePathname();
+    const session = useSession();
+    const handleSignout = async () => {
+        signOut({ callbackUrl: '/' });
+        // await signOut()
+        // router.push('/')
+    }
+
+    return (
+        <div>
+            <nav className='bg-primary p-4 text-white flex justify-between items-center'>
+                <Link href={'/'}>
+                    <div className=''>
+                        <Image src={logo} width={50} height={100} alt='Logo' className='r rounded-full block m-auto' priority/>
+                    </div>
+                </Link>
+                {
+                    session.status === 'authenticated' ? (
+                        <div className='text-center flex gap-5 items-center'>
+                            <div className='text-xl flex items-center justify-center'>
+                                <MdWavingHand className='text-2xl'/>
+                                &nbsp;&nbsp; <span className='text-amber-300 text-3xl font-serif'>{session.data?.user?.username || session.data?.user?.name.split(' ')[0]}</span>
+                            </div>
+                            <Link href={'/personal-space'} className={`${path === '/personal-space' ? 'rounded-md bg-secondary text-black px-4 py-2' : 'text-white'}`}>Personal-Space</Link>
+                            <button onClick={handleSignout} className='px-4 py-2 bg-secondary text-black rounded-md'>Sign Out</button>
+                        </div>
+                    ) : (
+                        <div>
+                            <ul className='flex items-center gap-14'>
+                                <li>
+                                    <Link href={'/'} className={`${path === '/' ? 'rounded-lg bg-white text-primary border px-4 py-2' : 'text-white'}`}>Home</Link>
+                                </li>
+                                <li>
+                                    <Link href={'/register'} className={`${path === '/register' ? 'rounded-lg bg-white text-primary border px-4 py-2' : 'text-white'}`}>Register</Link>
+                                </li>
+                                <li>
+                                    <Link href={'/login'} className={`${path === '/login' ? 'rounded-lg bg-white text-primary border px-4 py-2' : 'text-white'}`}>Login</Link>
+                                </li>
+                                <li>
+                                    <Link href={'/public-diaries'} className={`${path === '/public-diaries' ? 'rounded-lg bg-white text-primary border px-4 py-2' : 'text-white'}`}>Public-Diaries</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    )
+                }
+
+            </nav>
+        </div>
+    )
 }
 
 export default Navbar
